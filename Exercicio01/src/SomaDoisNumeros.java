0e1278f6083a08a6020e5e478e9cbe1d5a38f88a
vinitava07 <84585919+vinitava07@users.noreply.github.com>
2023-03-02 11:38:22 -0300

    Adicionando arquivos

---
+++ Exercicio01/src/SomaDoisNumeros.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+import java.util.Scanner;
+
+public class SomaDoisNumeros {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int num1,num2,total;
+		System.out.println("Digite um número");
+		num1 = sc.nextInt();
+		System.out.println("Digite outro número");
+		num2 = sc.nextInt();
+		total = num1+num2;
+		System.out.println("A soma dos números é: "+total);
+		sc.close();
+
+		
+
+	}
+
+}
